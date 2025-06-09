e8beff5a0300747f38a7b64351b65cc43b51186c
djiwandou-p <djiwandou@gmail.com>
2025-06-09 14:45:02 +0700

    feat: Add 2e-changing-elements-styles

---
+++ 2e-changing-elements-styles/js/app.js
@@ -0,0 +1,5 @@
+document.getElementById('changeStyleBtn').onclick = function() {
+    document.getElementById('colorBox').style.background = '#ff5733';
+    document.getElementById('colorBox').style.color = '#fff';
+  };
+  
