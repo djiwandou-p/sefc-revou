6d6f6667702c1db3fcfc62d67322ea9d911b3f7c
djiwandou-p <djiwandou@gmail.com>
2025-06-10 20:34:22 +0700

    feat: Add dark mode function

--- 9-git-workflow-simulation/js/app.js
+++ 9-git-workflow-simulation/js/app.js
@@ -1,4 +1,8 @@
 document.getElementById('colorBtn').onclick = function() {
     document.body.style.background = '#e0f7fa';
   };
-  
+
+  document.getElementById('darkModeBtn').onclick = function() {
+    document.body.classList.toggle('dark-mode');
+    document.querySelector('header').classList.toggle('dark-mode');
+  };
